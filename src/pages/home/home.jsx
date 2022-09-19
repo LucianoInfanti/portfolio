f9c5cce2d0ee3468dd7d95eb974d4b8bff766ccf
LucianoInfanti <lucianoinfanti369@gmail.com>
2022-09-19 08:53:50 -0300

    fixing the title issue

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,45 +11,41 @@ export default function Home() {
   const articles = [
     {
       id: 1,
-      title: () => (<> My take on <em className="italic">Svelte</em></> ),
+      title: () => (
+        <>
+          My title <em className="italic">Svelte</em>
+        </>
+      ),
       time: "05",
       folder: "article1",
       route: "my-take-on-svelte",
       image: "image.png",
-      position: 3,
     },
     {
       id: 2,
-      title: () => (<> My take on <em className="italic">Svelte</em></> ),
+      title: () => (
+        <>
+          <em className="italic">Protopie</em> good practices
+        </>
+      ),
       time: "09",
       folder: "article1",
       route: "my-take-on-svelte",
       image: "image.png",
-      position: 0,
     },
     {
       id: 3,
-      title: () => (<> My take on <em className="italic">Svelte</em></> ),
+      title: () => (
+        <>
+          On design <em className="italic">entropy</em>
+        </>
+      ),
       time: "04",
       folder: "article1",
       route: "my-take-on-svelte",
       image: "image.png",
-      position: 2,
     },
   ];
-
-  // const processedTitle = () => {
-  //   for (let i = 0; i < articles.length; i++) {
-  //     const titleSplitted = articles[0].title.split(" ");
-  //     for (let i = 0; i < titleSplitted.length; i++) {
-  //       const position = articles[i].position;
-  //       const whichWord = titleSplitted[position];
-  //       const applyItalic = "" + <em className="italic">{whichWord}</em>;
-  //     }
-  //   }
-  // };
-
-  // var processedTitle = 'Titulo <em className"italic">' + article.title + '</em>'
 
   return (
     <div className="container">
@@ -87,54 +83,7 @@ export default function Home() {
               />
             </motion.div>
           ))}
-          {/* <Article
-              title={
-                <>
-                  My take on <em className="italic">Svelte</em>
-                </>
-              }
-              time="05"
-              folder="article1"
-              route="my-take-on-svelte"
-              image="main.png"
-            />
-            <Article
-              title={
-                <>
-                  <em className="italic">Protopie</em> good practices
-                </>
-              }
-              folder="article4"
-              route="protopie-page-scroll-position"
-              image="main.png"
-              time="04"
-            />
-            <Article
-              title={
-                <>
-                  My take on <em className="italic">Svelte</em> as a{" "}
-                  <em className="italic">designer</em>
-                </>
-              }
-              folder="article4"
-              route="protopie-page-scroll-position"
-              image="main.png"
-              time="09"
-            />
-            <Article
-              title="Try subtracting before adding"
-              folder="article4"
-              route="protopie-page-scroll-position"
-              image="main.png"
-              time="04"
-            />
-            <Article
-              title="Design Entropy"
-              folder="article4"
-              route="protopie-page-scroll-position"
-              image="main.png"
-              time="21"
-            /> */}
+
           {articles.length > 3 && <div className="spacer"></div>}
         </div>
       </div>
